bc3f89c5b33928ff5c2cc6d43ef77f652ecf6b49
ArnoAnsems <43150430+ArnoAnsems@users.noreply.github.com>
2022-01-10 22:44:01 +0100

    Introduce game specific SavedGameConverter classes

---
+++ src/Abyss/SavedGameConverterAbyss.h
@@ -0,0 +1,33 @@
+// Copyright (C) 2022 Arno Ansems
+// 
+// This program is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published by 
+// the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version. 
+// 
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+// GNU General Public License for more details. 
+// 
+// You should have received a copy of the GNU General Public License 
+// along with this program.  If not, see http://www.gnu.org/licenses/ 
+
+//
+// SavedGameConverterAbyss
+//
+// Conversion of saved games from DOS format to CatacombGL; specifically for Catacomb Abyss
+//
+#pragma once
+
+#include "../Engine/ISavedGameConverter.h"
+
+class SavedGameConverterAbyss : public ISavedGameConverter
+{
+public:
+    SavedGameConverterAbyss();
+    ~SavedGameConverterAbyss();
+    DosFormatConfig GetDosFormatConfig() const override;
+};
+
+
