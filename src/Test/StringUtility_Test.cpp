57277c1b54b615f83112244044e5186267bd90de
ArnoAnsems <43150430+ArnoAnsems@users.noreply.github.com>
2025-12-27 20:59:41 +0100

    Add class test for StringUtility

---
+++ src/Test/StringUtility_Test.cpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2025 Arno Ansems
+// 
+// This program is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published by 
+// the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version. 
+// 
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+// GNU General Public License for more details. 
+// 
+// You should have received a copy of the GNU General Public License 
+// along with this program.  If not, see http://www.gnu.org/licenses/ 
+
+#include "StringUtility_Test.h"
+#include "../Engine/StringUtility.h"
+
+StringUtility_Test::StringUtility_Test()
+{
+
+}
+
+StringUtility_Test::~StringUtility_Test()
+{
+
+}
+
+TEST(StringUtility_Test, IntToHexadecimal)
+{
+	EXPECT_EQ("0x0", StringUtility::IntToHexadecimal(0u));
+	EXPECT_EQ("0xA", StringUtility::IntToHexadecimal(10u));
+	EXPECT_EQ("0xFFFF", StringUtility::IntToHexadecimal(65535u));
+	EXPECT_EQ("0x10000", StringUtility::IntToHexadecimal(65536u));
+}
